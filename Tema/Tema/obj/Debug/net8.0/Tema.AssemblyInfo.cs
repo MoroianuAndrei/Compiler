d3d6efb7d137f0cd1da4c17fcd273e148338c071
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2025-01-11 19:50:17 +0200

    This is the final code

--- Tema/Tema/obj/Debug/net8.0/Tema.AssemblyInfo.cs
+++ Tema/Tema/obj/Debug/net8.0/Tema.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4772c88646b4e45314ebb87a3ab7a231f4bce03e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7be13199bbdd11f4b19437d4c042961399569f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
